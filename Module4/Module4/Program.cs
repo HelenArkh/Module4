94869daa4b6931784fdb304d3d54315d70db44c6
HelenArkh <alena.arkh@gmail.com>
2020-12-20 13:46:25 +0300

    Создание проекта и задание 4.1.5

---
+++ Module4/Module4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            double X;
+            double Y;
+
+            bool C = (A < B) | (X > Y);
+        }
+    }
+}
