fe46980425e031574f9143eec9359e5582dc7606
HelenArkh <alena.arkh@gmail.com>
2020-12-20 20:21:06 +0300

    Добавила программу по заданию 4.1.15

--- Module4/Module4/Program.cs
+++ Module4/Module4/Program.cs
@@ -6,12 +6,32 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-            int A;
-            int B;
-            double X;
-            double Y;
+            var inv = true;
+            var result = !inv;
 
-            bool C = (A < B) | (X > Y);
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            FavoriteColor.MyFavoriteColor();
+
+
+            var a = 6;
+            var b = 7;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else if (b < 10)
+            {
+                    Console.WriteLine("Значение b = {0} меньше 10", b);
+            }
+            else
+            {
+                Console.WriteLine("Значение b = {0} больше 10", b);
+            }
+
+
+            }
         }
     }
-}
